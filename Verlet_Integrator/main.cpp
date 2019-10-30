b53d951c69163d4751859d1d4aa2c8bb966fc457
[Eudald] <47557352+Hevne@users.noreply.github.com>
2019-10-30 13:58:59 +0100

    Base code done

---
+++ Verlet_Integrator/main.cpp
@@ -0,0 +1,20 @@
+//VERLET INTEGRATION METHOD
+
+#include "p2Defs.h"
+#include "p2List.h"
+#include "IntegratorFunctions.h"
+
+void main()
+{
+	VerletIntegrator integrator;
+
+	integrator.InitPoint();
+	integrator.updatePoints();
+	
+
+
+
+
+
+
+}
